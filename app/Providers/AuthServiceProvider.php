d2e4acd5dba39f9dcba96f3218c99ee8437aac69
pengpn <312924860@qq.com>
2017-02-28 07:16:19 +0000

    Finish user edit , update,index,and destroy actions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,13 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use App\Models\User;
+use App\Policies\UserPolicy;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        User::class => UserPolicy::class,
     ];
 
     /**
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->toArray());
+
+        $user = User::find(1);
+        $user->name = 'PENGPN';
+        $user->email = '312924860@qq.com';
+        $user->password = bcrypt('6522424');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
