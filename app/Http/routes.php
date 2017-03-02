6d46ff0ffed0ccb9eb9547879c05264dc4159659
pengpn <312924860@qq.com>
2017-03-02 05:27:00 +0000

    Add password resets & email configuration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,11 @@ delete('logout','SessionsController@destroy')->name('logout');
 
 get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
 
+get('password/email','Auth\PasswordController@getEmail')->name('password.reset');
+post('password/email','Auth\PasswordController@postEmail')->name('password.edit');
+get('password/reset/{token}','Auth\PasswordController@getReset')->name('password.edit');
+post('password/reset','Auth\PasswordController@postReset')->name('password.update');
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
