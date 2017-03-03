fe9f54f9eef3c88c3f1d5529bc0036ca54f18a76
pengpn <312924860@qq.com>
2017-03-03 01:56:46 +0000

    fixed routes bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,7 @@ delete('logout','SessionsController@destroy')->name('logout');
 get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
 
 get('password/email','Auth\PasswordController@getEmail')->name('password.reset');
-post('password/email','Auth\PasswordController@postEmail')->name('password.email');
+post('password/email','Auth\PasswordController@postEmail')->name('password.reset');
 get('password/reset/{token}','Auth\PasswordController@getReset')->name('password.edit');
 post('password/reset','Auth\PasswordController@postReset')->name('password.update');
 
